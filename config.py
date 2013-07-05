67738ed729cf582c15ecb665150abe0a3489eb24
tiotao <yiwen@nus.edu.sg>
2013-07-05 15:48:59 +0800

    Initial commit

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+CSRF_ENABLED = True
+SECRET_KEY = 'klsd920612'
+WHOOSH_BASE = os.path.join(basedir, 'search.db')
+POSTS_PER_PAGE = 3
+MAX_SEARCH_RESULTS = 50
+OPENID_PROVIDERS = [
+    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
