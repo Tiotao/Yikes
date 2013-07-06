9a42c3e04d93c6abc33f75fddab54e8c513c38e8
root <evan@tiotao.info>
2013-07-06 22:28:54 +0800

    修改：      app/views.py 	修改：      run.py 	修改：      setup.py

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
 #!flask/bin/python
+
+from migrate.versioning import api
+from config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_MIGRATE_REPO
+from app import db
 from app import app
-app.run(debug=True)
+import os.path
+db.create_all()
+if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+else:
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
+port = int(os.environ.get('PORT',5000))
+app.run(host='0.0.0.0', port=port)
