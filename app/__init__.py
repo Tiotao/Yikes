53c7ae90d56c569e46146d2e5e3714d22c6e6e4f
tiotao <yiwen@nus.edu.sg>
2013-07-06 22:44:59 +0800

    修改：      app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,16 +5,17 @@ from flask.ext.heroku import Heroku
 from config import basedir
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+engine = create_engine('')
 
 app = Flask(__name__)
 heroku = Heroku(app)
 app.config.from_object('config')
-db = SQLAlchemy(app)
+db = create_engine('postgresql://nqblmxqesvbcjb:Q12QFIfpSaNqOJtp0KShFwmFN5@ec2-107-20-201-165.compute-1.amazonaws.com:5432/d7vao9hm2qoc3a')
 lm = LoginManager()
 lm.setup_app(app)
 lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views, models
-
 
