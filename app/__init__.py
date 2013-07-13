d68ac7478fe557705118ac016cfcee9d5cc2ff33
tiotao <yiwen@nus.edu.sg>
2013-07-14 01:29:08 +0800

    1

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask.ext.openid import OpenID
 from config import basedir
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from momentjs import momentjs
+
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -13,9 +15,11 @@ lm.setup_app(app)
 lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
-from momentjs import momentjs
+
 app.jinja_env.globals['momentjs'] = momentjs
 
 from app import views, models
+
+
 
 
