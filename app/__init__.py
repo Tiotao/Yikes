953bd44a5e1e8bb02183c6502aa5ef013c4ad9ae
root <evan@tiotao.info>
2013-07-15 11:16:38 +0800

    yes

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from config import basedir
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from momentjs import momentjs
-
+from flask_oauth import OAuth
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -18,8 +18,16 @@ oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 app.jinja_env.globals['momentjs'] = momentjs
 
+
+oauth = OAuth()
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key='F180236948816069',
+    consumer_secret='becbcdbb934c58dae6e4305798d89b00',
+    request_token_params={'scope': 'email'}
+)
+
 from app import views, models
-
-
-
-
