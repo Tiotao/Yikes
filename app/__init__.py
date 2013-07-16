90866604bfc6a86b3d7e2eaf66dac10e8907f828
tiotao <yiwen@nus.edu.sg>
2013-07-17 00:59:44 +0800

    for sina weibo authorisation

--- app/__init__.py
+++ app/__init__.py
@@ -30,22 +30,5 @@ facebook = oauth.remote_app('facebook',
     request_token_params={'scope': 'email'}
 )
 
-twitter = oauth.remote_app('twitter',
-    # unless absolute urls are used to make requests, this will be added
-    # before all URLs.  This is also true for request_token_url and others.
-    base_url='https://api.twitter.com/1/',
-    # where flask should look for new request tokens
-    request_token_url='https://api.twitter.com/oauth/request_token',
-    # where flask should exchange the token with the remote application
-    access_token_url='https://api.twitter.com/oauth/access_token',
-    # twitter knows two authorizatiom URLs.  /authorize and /authenticate.
-    # they mostly work the same, but for sign on /authenticate is
-    # expected because this will give the user a slightly different
-    # user interface on the twitter side.
-    authorize_url='https://api.twitter.com/oauth/authenticate',
-    # the consumer keys from the twitter application registry.
-    consumer_key='rVp6vBKbOqwe3p67vbLgLg',
-    consumer_secret='viLPVMsUPaQIuV7KgD0i2DlJX2HXllh7Wyr9eETz6Q'
-)
 
 from app import views, models
